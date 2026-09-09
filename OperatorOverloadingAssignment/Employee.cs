f5d7c77f7f211bdd8ec6922198eefe6b1972b293
User <User@DESKTOP-86U9LE4>
2026-09-09 18:28:02 -0400

    Add project files.

---
+++ OperatorOverloadingAssignment/Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace OperatorOverloadingAssignment
+{
+    // This class stores employee information
+    public class Employee
+    {
+        // Employee ID number
+        public int Id { get; set; }
+
+        // Employee first name
+        public string FName { get; set; } = "";
+
+        // Employee last name
+        public string LName { get; set; } = "";
+
+        // Compare employees using ID
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            if (ReferenceEquals(emp1, emp2))
+            {
+                return true;
+            }
+
+            if (emp1 is null || emp2 is null)
+            {
+                return false;
+            }
+
+            return emp1.Id == emp2.Id;
+        }
+
+        // Opposite of ==
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return !(emp1 == emp2);
+        }
+
+        // Check if two Employee objects are equal
+        public override bool Equals(object? obj)
+        {
+            if (obj is Employee employee)
+            {
+                return Id == employee.Id;
+            }
+
+            return false;
+        }
+
+        // Create a hash code using the ID
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
---
+++ OperatorOverloadingAssignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OperatorOverloadingAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create first employee
+            Employee emp1 = new Employee();
+
+            // Give values to emp1
+            emp1.Id = 1001;
+            emp1.FName = "John";
+            emp1.LName = "Smith";
+
+            // Create second employee
+            Employee emp2 = new Employee();
+
+            // Give values to emp2
+            emp2.Id = 1001;
+            emp2.FName = "Jane";
+            emp2.LName = "Doe";
+
+            // Compare employees
+            bool isEqual = emp1 == emp2;
+            bool isNotEqual = emp1 != emp2;
+
+            // Show employee details
+            Console.WriteLine($"Employee 1: {emp1.FName} {emp1.LName}");
+            Console.WriteLine($"Employee 2: {emp2.FName} {emp2.LName}");
+
+            // Show comparison results
+            Console.WriteLine($"emp1 == emp2: {isEqual}");
+            Console.WriteLine($"emp1 != emp2: {isNotEqual}");
+
+            Console.ReadLine();
+        }
+    }
+}
